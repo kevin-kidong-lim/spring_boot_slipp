8db17a4d772965cb5ec9ce5993d9dd5c014a3cfc
kevinkidonglim <kevin.kidong.lim@gmail.com>
2020-06-03 15:20:11 -0500

    1

--- src/main/java/net/slipp/web/WelcomeController.java
+++ src/main/java/net/slipp/web/WelcomeController.java
@@ -1,5 +1,8 @@
 package net.slipp.web;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,11 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/helloworld")
-	public String welcome(String name, int age, Model model) {
+	public String welcome( Model model) {
 		System.out.println(" this is welcome ");
-		System.out.println(" name " + name);
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+		List<MyModel> repo = Arrays.asList(new MyModel("kein"),new MyModel("kein2"));
+		model.addAttribute("repo", repo);
 		return "welcome";
 	}
 }
