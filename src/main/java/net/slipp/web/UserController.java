61beb6b960e5ba0d78f6061568c08bd266d115ea
kevinkidonglim <kevin.kidong.lim@gmail.com>
2020-06-04 11:58:34 -0500

    0604 commit

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -3,24 +3,41 @@ package net.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")  // 중복제거 , 아래 메소드에 있는걸 공용으로 사용할때 사용 ..
 public class UserController {
-	private List<User> users = new ArrayList<User>();
+
+	@Autowired
+	private UserRepository userRepository;
 	
-	@PostMapping("/create")
+	
+	@PostMapping("")
 	public String create(User user) {
-		System.out.println("userId" + user);
-		users.add(user);
-		return "redirect:/list";
+		System.out.println("aa userId" + user);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
+	
+	@GetMapping("/update")
+	public String update(User user) {
+		System.out.println("aa userId" + user);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
 }
