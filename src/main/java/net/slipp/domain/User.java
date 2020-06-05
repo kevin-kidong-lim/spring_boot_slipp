e0602a3ea999f30a9b3dbfcd3e6bf5ccc47bc6ee
kevinkidonglim <kevin.kidong.lim@gmail.com>
2020-06-05 11:59:07 -0500

    qna create commit;  0605

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -8,53 +8,67 @@ import javax.persistence.Id;
 @Entity
 public class User {
 	@Id
-	@GeneratedValue  // auto sequence
+	@GeneratedValue // auto sequence
 	private Long id;
-	@Column(nullable=false, length=20, unique=true)
+	@Column(nullable = false, length = 20, unique = true)
 	private String userId;
 
 	private String password;
 	private String name;
 	private String email;
-	
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
+	public boolean matchPassword(String newPassword) {
+		if (newPassword == null) {
+			return false;
+		}
+		return newPassword.equals(password);
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", password=" + password + ", name=" + name + ", email=" + email + "]";
 	}
+
 	public void update(User updateUser) {
 		// TODO Auto-generated method stub
 		this.password = updateUser.password;
 		this.name = updateUser.name;
 		this.email = updateUser.email;
-		
+
 	}
-	public Long getId() {
-		return id;
+
+	public boolean matchId(Long newId) {
+		if (newId == null) {
+			return false;
+		}
+		return newId.equals(id);
 	}
 	public String getUserId() {
 		return userId;
 	}
-	public String getPassword() {
-		return password;
-	}
+
 	public String getName() {
 		return name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
-	
-	
+
 }
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -33,17 +33,17 @@ public class UserController {
 		if (user == null) {
 			return "redirect:/users/loginForm";
 		}
-		if ( !password.equals(user.getPassword())) {
+		if ( !user.matchPassword(password)) {
 			return "redirect:/users/loginForm";
 		}
 
-		session.setAttribute("sessionedUser",  user);
+		session.setAttribute(HttpSessionUtils.USER_SESSION_KEY,  user);
 		return "redirect:/";
 	}
 	
 	@GetMapping("/logout")
 	public String logout( HttpSession session) {
-		session.removeAttribute("sessionedUser");
+		session.removeAttribute(HttpSessionUtils.USER_SESSION_KEY);
 		return "redirect:/";
 	}
 	
@@ -66,11 +66,12 @@ public class UserController {
 	
 	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model,HttpSession session) {
-		User sessionedUser = (User)session.getAttribute("sessionedUser");
-		if (sessionedUser == null ) {
+		
+		if (HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-		if ( !id.equals(sessionedUser.getId())) {
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		if ( ! sessionedUser.matchId(id)) {
 			throw new IllegalStateException("Not matched user");
 		}
 			
@@ -83,11 +84,11 @@ public class UserController {
 	
 	@PostMapping("/{id}")
 	public String update(@PathVariable Long id, User updateUser,HttpSession session) {
-		User sessionedUser = (User)session.getAttribute("sessionedUser");
-		if (sessionedUser == null ) {
+		if (HttpSessionUtils.isLoginUser(session)) {
 			return "redirect:/users/loginForm";
 		}
-		if ( !id.equals(sessionedUser.getId())) {
+		User sessionedUser = HttpSessionUtils.getUserFromSession(session);
+		if ( !sessionedUser.matchId(id) ) {
 			throw new IllegalStateException("Not matched user");
 		}
 		
